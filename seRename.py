b20d79af4921face121a07c41a9a1399350ca081
Alister Brumley <ali.b912@gmail.com>
2024-06-29 06:23:29 +1000

    renamed

--- seRename.py
+++ seRename.py
@@ -1,3 +1,28 @@
+#
+#
+# THIS SCRIPT RENAMES FILES IN SXXEXX ORDER
+# TO BE USED FOR SHOWS WHERE ALL EPISODES ARE S01EXX
+# PUT EPISODES INTO SEASON FOLDERS AND THEN RUN FROM SHOW FOLDER
+# EG
+# TV_SHOW/   < RUN HERE
+#   |____S1/
+#   |  |____SHOW_S01E01.MP4
+#   |  |____SHOW_S01E02.MP4
+#   |____S2/
+#      |____SHOW_S01E03.MP4
+#      |____SHOW_S01E04.MP4
+#
+#
+# WILL BECOME
+# TV_SHOW/
+#   |____S1/
+#   |  |____SHOW_S01E01.MP4
+#   |  |____SHOW_S01E02.MP4
+#   |____S2/
+#      |____SHOW_S02E01.MP4
+#      |____SHOW_S02E02.MP4
+#
+#
 import re
 from pathlib import Path
 
@@ -17,7 +42,7 @@ for file in filelist:
     series = "S0" + seriesCount
     episode = "E0" + str(episodeCount)
     renamed = re.sub(r"S0\dE\d+", series + episode, str(file))
-    # file.rename(renamed)
+    file.rename(renamed)
     print(renamed)
     episodeCount += 1
 
